9dce25eb288df9e175b4cedf09090a60892af626
matheushenrique <matheus.henriquepr@gmail.com>
2024-04-20 12:47:27 -0300

    Initial commit

---
+++ README copy.md
@@ -0,0 +1,13 @@
+Tools:
+
+  Docker
+  kibectl
+  K3D, Kind, Minikube
+  Lens
+  Helm
+  terraform
+
+
+Commands
+
+  npx prisma db seed
--- prisma/migrations/20240327140503_create_check_ins_table/migration.sql
+++ prisma/migrations/20240327140503_create_check_ins_table/migration.sql
@@ -1,10 +1,9 @@
 -- CreateTable
 CREATE TABLE "check_ins" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "attendee_id" INTEGER NOT NULL,
-    CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "id" serial NOT NULL PRIMARY KEY,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "attendee_id" INTEGER NOT NULL
 );
-
+-- CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 -- CreateIndex
-CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"("attendee_id");
+-- CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"("attendee_id");
